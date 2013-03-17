221b7395b7f430df268c58d3b5202cbfc525017e
Roy van de Water <roy@integrumtech.com>
2013-03-16 17:03:11 -0700

    Everything else

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sink::Application.config.secret_key_base = '37cadf63fabf36ee421cff37175d2dad16e5191c116458f87c9fd343a60148f21c6b7659e62c7d679e9f1f286b7b8dc933965d36b81e7b8a42896e4478de9810'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sink::Application.config.session_store :encrypted_cookie_store, key: '_sink_session'
